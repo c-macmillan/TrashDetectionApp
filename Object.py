8198cab60fd56483524418f022b9e5343d1474a0
Nhi Nguyen <npnguyen8@dons.usfca.edu>
2023-04-26 09:52:46 -0700

    add id2bin mapping

--- Object.py
+++ Object.py
@@ -4,11 +4,23 @@ id2label = {
     2: 'glass',
     3:'metal',
     4: 'paper',
-    5: 'plastic'
+    5: 'plastic',
+    6: 'trash'
+}
+
+id2bin = {
+    0: 'recycle',
+    1: 'compost',
+    2: 'recycle',
+    3:'recycle',
+    4: 'recycle',
+    5: 'recycle',
+    6: 'landfill'
 }
 
 class Object:
     def __init__(self, class_id, probability, education=""):
         self.label = id2label[class_id]
+        self.bin = id2bin[class_id]
         self.probability = str(round(float(probability), 2) * 100) + "%"
         self.education = education
